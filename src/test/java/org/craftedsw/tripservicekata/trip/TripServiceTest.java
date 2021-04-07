b3b6434a142badeb0a57ab7d0ac209b70cf24645
nicholas-balcolm <nicholas.balcolm@gmail.com>
2021-04-07 23:50:46 +0000

    wip: add maven extension. Not working on first try

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,14 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
 public class TripServiceTest {
-	
+	@Test(expected = UserNotLoggedInException.class) public void
+    should_throw_when_not_logged_in(){
+        TripService tripService = new TripService();    
+        
+        tripService.getTripsByUser(null);
+    }
 }
